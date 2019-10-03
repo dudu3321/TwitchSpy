11dceeb96e96a5f841166503822c4feffdfa72d0
Kervin Tsai <kervin.tsai@collaborate.tw>
2019-10-03 13:58:04 +0800

    first commit

---
+++ TwitchSpy/Form.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TwitchSpy
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(465, 213);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 266);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ TwitchSpy/Form.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TwitchLib.Api;
+using TwitchLib.Api.Helix.Models.Users;
+using Newtonsoft.Json;
+using TwitchLib.Api.Helix.Models.Streams;
+
+namespace TwitchSpy
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private TwitchAPI API;
+        //追隨的人
+        private List<Follow> allFollows;
+        //有開台的人
+        private List<Stream> liveStreams;
+        private List<TwitchLib.Api.V5.Models.Users.User> liveUserData;
+        GetUsersFollowsResponse responseData;
+
+        public Form()
+        {
+            InitializeComponent();
+            init();
+            
+            ResetItems();
+            GetFollowing();
+        }
+
+        private void Timer_TimesUp(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+        }
+
+        public async Task GetFollowing()
+        {
+            await GetAllFollows();
+            await GetFollowsOnLiveAsync();
+            await GetOnLiveUserData();
+            UpdateListView();
+        }
+
+        private void init()
+        {
+            API = new TwitchAPI();
+            API.Settings.ClientId = ConfigurationManager.AppSettings["client_id"];
+
+            InitialListView();
+
+            timer1.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private void ResetItems()
+        {
+            allFollows = new List<Follow>();
+            liveStreams = new List<Stream>();
+            liveUserData = new List<TwitchLib.Api.V5.Models.Users.User>();
+        }
+
+        /// <summary>
+        /// 取得使用者追隨的帳號
+        /// </summary>
+        /// <returns></returns>
+        private async Task GetAllFollows()
+        {
+            while (true)
+            {
+                responseData = await API.Helix.Users.GetUsersFollowsAsync((responseData?.Pagination?.Cursor?.ToString() ?? null), null, 100, "18678591");
+                if (responseData != null)
+                {
+                    if (responseData.Pagination.Cursor == null) break;
+                    allFollows.AddRange(responseData.Follows.ToList());
+                }
+            }
+        }
+
+        /// <summary>
+        /// 檢查allFollows中哪些有開台
+        /// </summary>
+        /// <returns></returns>
+        private async Task GetFollowsOnLiveAsync()
+        {
+            try
+            {
+                var getParams = new List<KeyValuePair<string, string>>();
+                for (int i = 0; i < (Math.Ceiling(((double)allFollows.Count()/100))); i++)
+                {
+                    List<string> userIds = allFollows.Select(f => f.ToUserId).Skip(i*100).Take(100).ToList();
+                    liveStreams.AddRange((await API.Helix.Streams.GetStreamsAsync(first: 100, userIds: userIds)).Streams.ToList());
+                }
+            }
+            catch(Exception ex)
+            {
+                ErrorMethod(ex);
+            }
+        }
+
+        /// <summary>
+        /// 取得liveUsers內使用者資料
+        /// </summary>
+        /// <returns></returns>
+        private async Task GetOnLiveUserData()
+        {
+            try
+            {
+                var tasks = liveStreams.Select(async f =>
+                {
+                    liveUserData.Add(await API.V5.Users.GetUserByIDAsync(f.UserId));
+                });
+                await Task.WhenAll(tasks);
+            }
+            catch (Exception ex)
+            {
+                ErrorMethod(ex);
+            }
+        }
+
+        private void InitialListView()
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.LabelEdit = false;
+            listView1.FullRowSelect = true;
+            this.listView1.Columns.Add("ID", 100);
+            this.listView1.Columns.Add("Name", 100);
+            this.listView1.Columns.Add("標題",265);
+        }
+
+        private void UpdateListView()
+        {
+            this.listView1.BeginUpdate();
+            this.listView1.Items.Clear();
+            foreach (var f in liveUserData)
+            {
+                var stream = liveStreams.FirstOrDefault(x => x.UserId == f.Id);
+                var i = new ListViewItem();
+                i.Text = f.Name;
+                i.SubItems.Add(f.DisplayName);
+                i.SubItems.Add(stream.Title);
+                this.listView1.Items.Add(i);
+            }
+            this.listView1.EndUpdate();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            ResetItems();
+            GetFollowing();
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            ResetItems();
+            GetFollowing();
+        }
+
+        private void ErrorMethod(Exception ex)
+        {
+            var a = ex;
+        }
+    }
+}
